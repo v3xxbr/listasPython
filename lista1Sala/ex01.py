85607f432cc8b060fac88ec6bedab7243014a5c1
v3xxbr <blaackhole07@gmail.com>
2025-10-08 15:56:11 -0300

    Correção Exercício 1
    agora adicionei a exibição dos valores de velocidade, tempo e distância.

--- lista1Sala/ex01.py
+++ lista1Sala/ex01.py
@@ -5,4 +5,7 @@ ds = vm*dt
 
 litros = ds/12
 
+print(f'Velocidade Média: {vm:.2f} km/h')
+print(f'Tempo: {dt:.2f} h')
+print(f'Distância: {ds:.2f} km')
 print(f'Foram gastos {litros:.2f} litros nessa viagem.')
