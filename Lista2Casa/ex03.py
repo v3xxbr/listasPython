24adfd45831cab0b10605245cc7066bb9b2b9e72
v3xxbr <blaackhole07@gmail.com>
2025-10-19 11:37:21 -0300

    Lista2Casa
    Completa

--- Lista2Casa/ex03.py
+++ Lista2Casa/ex03.py
@@ -1,2 +1,10 @@
-import math
+media = float(input('Digite a média: '))
+media = round(media,2)
+dec = media - int(media)
 
+media=int(media)
+
+if (dec*100) > 50:
+    media+=1
+
+print(f'A média arredondada é {media:.2f}')
