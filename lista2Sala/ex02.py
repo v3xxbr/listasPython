b48ecdfed459fb92cf647fdb9584bdb37b6cd7fa
v3xxbr <blaackhole07@gmail.com>
2025-10-19 12:29:47 -0300

    Correção Ex02

--- lista2Sala/ex02.py
+++ lista2Sala/ex02.py
@@ -7,10 +7,8 @@ if(media>=6):
     print(f'Sua média é {media:.2f} ! Você foi aprovado!')
 else:
     exame = float(input('Informe a nota do exame: '))
-
-media +=5
-
-if(media >=5):
-    print(f'Sua média é {media:.2f} ! Você foi aprovado em exame!')
-else:
-    print(f'Sua média é {media:2f} ! Você foi reprovado!')
+    media = (exame+media)/2
+    if(media >=5):
+        print(f'Sua média é {media:.2f} ! Você foi aprovado em exame!')
+    else:
+        print(f'Sua média é {media:2f} ! Você foi reprovado!')
