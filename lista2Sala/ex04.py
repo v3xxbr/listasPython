108c1fa3beaaa642ba3917f1fdd49aef9d279fb4
v3xxbr <blaackhole07@gmail.com>
2025-10-19 11:58:04 -0300

    correção do exercício 4

--- lista2Sala/ex04.py
+++ lista2Sala/ex04.py
@@ -3,11 +3,11 @@ b = float(input('Digite o segundo lado: '))
 c = float(input('Digite o terceiro lado: '))
 
 if a<b+c and b<a+c and c<a+b:
-    if a==b or b==c or c==a:
-        print('Isóceles.')
-    elif a!=b or a!=c or b!=c:
-        print('Escaleno.')
-    elif a==b and b==c:
+    if a==b and b==c:
         print('Equilátero.')
+    elif a==b or b==c or c==a:
+        print('Isóceles.')
+    elif a!=b and a!=c and b!=c:
+        print('Escaleno.')
 else:
     print('Não é um triângulo.')
