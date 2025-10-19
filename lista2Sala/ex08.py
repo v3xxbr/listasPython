69231235761b68bda0450003244bbfa6e99f1f53
v3xxbr <blaackhole07@gmail.com>
2025-10-19 12:21:31 -0300

    Correção do Exercício 8

--- lista2Sala/ex08.py
+++ lista2Sala/ex08.py
@@ -1,10 +1,8 @@
-a = int(input('Digite o primeiro valor: '))
-b = int(input('Digite o segundo valor: '))
-c = int(input('Digite o terceiro valor: '))
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o primeiro número: '))
+c = int(input('Digite o terceiro número: '))
+div = [a, b, c]
 
-if a%6==0:
-    print(f'{a} é divisível por 2 e por 3.')
-if b%6==0:
-    print(f'{b} é divisível por 2 e por 3.')
-if c%6==0:
-    print(f'{c} é divisível por 2 e por 3.')
+for y in range(3):
+    if div[y]%6 == 0:
+        print(f'{div[y]} é divisível por 2 e por 3')
